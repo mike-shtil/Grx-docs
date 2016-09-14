d79fcafec6b967407550dbe2ace7a7cca2be8537
mike-shtil <mike002@gmail.com>
2016-09-14 21:38:11 +0300

    split the docs/demo from main Grx project

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+
+//default task
+gulp.task('default', ['demo', 'watch']);
+
+//watch Files For Changes
+gulp.task('watch', function() {
+  return gulp.watch('styles/**/*.scss', ['demo']);
+});
+
+gulp.task('demo', function(){
+  return gulp.src('demo/style/**/*.scss')
+     .pipe($.plumber({errorHandler: function(error){
+        console.log(error);
+        this.emit('end');
+     }}))
+     .pipe($.sourcemaps.init())
+     .pipe($.sass({errLogToConsole: true, sourcemap: true, style: 'compact'}))
+     .pipe($.autoprefixer({browsers: ['last 1 version', 'iOS 6'], cascade: false}))
+     .pipe($.sourcemaps.write({includeContent: false, sourceRoot: '.'}))
+     //.pipe($.cssnano({discardDuplicates: false})) //optional minification
+     .pipe(gulp.dest('demo/style'));
+});
